a650a2923f991a6f00212b26fef431ff915dfec5
Jennifer Evans <jenevans3ten@gmail.com>
2019-06-23 22:47:43 -0400

    updated courses model with one-to-many relationship to instructor

--- Courses/Models/Course.cs
+++ Courses/Models/Course.cs
@@ -11,6 +11,9 @@ namespace Courses.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public virtual int InstructorId { get; set; }
+        public virtual Instructor Instructor { get; set; }
+
         public Course()
         {
         }
