91ba1af3bf294ed9384985e5df2cca90c0de87b6
Jen E <jenevans3ten@gmail.com>
2020-03-01 19:04:38 -0500

    add studentcourse controller with dependency injection

---
+++ Courses/Controllers/StudentCourseController.cs
@@ -0,0 +1,20 @@
+﻿using Courses.Models;
+using Courses.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses.Controllers
+{
+    public class StudentCourseController : Controller
+    {
+        IRepository<StudentCourse> studentCourseRepo;
+        
+        public StudentCourseController(IRepository<StudentCourse> studentCourseRepo)
+        {
+            this.studentCourseRepo = studentCourseRepo;
+        }
+    }
+}
