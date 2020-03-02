08509a101cc78987db183a4626f042548583b426
Jen E <jenevans3ten@gmail.com>
2020-03-01 19:04:01 -0500

    added studentcourse repository

---
+++ Courses/Repositories/StudentCourseRepository.cs
@@ -0,0 +1,40 @@
+﻿using Courses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses.Repositories
+{
+    public class StudentCourseRepository : IRepository<StudentCourse>
+    {
+        private UniversityContext db;
+
+        public StudentCourseRepository(UniversityContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(StudentCourse studentCourse)
+        {
+            db.StudentCourses.Add(studentCourse);
+            db.SaveChanges();
+        }
+
+        public void Delete(StudentCourse studentCourse)
+        {
+            db.StudentCourses.Remove(studentCourse);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<StudentCourse> GetAll()
+        {
+            return db.StudentCourses;
+        }
+
+        public StudentCourse GetById(int id)
+        {
+            return db.StudentCourses.Single(c => c.Id == id);
+        }
+    }
+}
